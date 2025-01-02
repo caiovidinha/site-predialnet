70a0f4d2d09e13badb325696e190b435e3fbaa38
Caio Vidinha <caiovidinha@id.uff.br>
2025-01-02 16:25:13 -0300

    ssl

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -35,7 +35,7 @@ const sendEmail = async(to,subject,body) => {
         "subject": subject,
         "content": body
     }
-    const email = await instance.post('https://backend.predialnet.com.br/emails', data)
+    const email = await instance.post('https://appgw.predialnet.com.br/emails', data)
     if(!email) return {error: "Erro ao enviar o e-mail"}
     return email
 }
