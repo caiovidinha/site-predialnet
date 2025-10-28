93712356c362822136b943f850d9d499b66e4ea8
caiovidinha <caiovidinha@id.uff.br>
2025-10-28 08:29:01 -0300

    remove outubro rosa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,13 +27,6 @@ const Nav = () => {
                 </p>
               </Link>
             </li>
-            <li>
-              <Link href="/outubro-rosa">
-                <p className="text-[#9e9e9e] hover:text-[#9c0004] text-sm font-semibold duration-150 ease-in-out hover:border-b-[#9c0004] hover:border-solid hover:border-b-2">
-                  Outubro Rosa
-                </p>
-              </Link>
-            </li>
             {/* <li>
               <Link href="/guerreiro-bom-de-bola">
                 <p className="text-[#9e9e9e] hover:text-[#9c0004] text-sm font-semibold duration-150 ease-in-out hover:border-b-[#9c0004] hover:border-solid hover:border-b-2">
@@ -94,11 +87,6 @@ const Nav = () => {
           <Link href="/">
             <p className="text-[#9c0004] text-xs font-semibold whitespace-nowrap">
               Para sua casa
-            </p>
-          </Link>
-          <Link href="/outubro-rosa">
-            <p className="text-[#9e9e9e] hover:text-[#9c0004] text-xs font-semibold whitespace-nowrap">
-              Outubro Rosa
             </p>
           </Link>
           {/* <Link href="/guerreiro-bom-de-bola">
